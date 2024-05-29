1b464c13098340a414037f562068e98b3116f794
Szymon Brodawka <szymbrod@gmail.com>
2024-05-29 12:29:15 +0200

    Center caculator in the middle verticaly

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 
 function App() {
   return (
-  <div className="container mx-auto flex flex-row justify-center">
-    <div className="bg-neutral-700 text-neutral-100 text-2xl grid grid-cols-4 gap-6 rounded-3xl p-5 mt-20">
-      <div className=" col-span-4 h-[10rem]"></div>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">AC</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">()</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">%</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">/</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">7</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">8</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">9</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">x</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">4</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">5</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">6</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">-</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">1</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">2</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">3</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">+</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">0</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">,</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">C</button>
-      <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">=</button>
+    <div className="container mx-auto flex justify-center">
+      <div className="bg-neutral-700 text-neutral-100 text-2xl grid grid-cols-4 gap-6 rounded-3xl p-5">
+        <div className="col-span-4 h-[8rem]"></div>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">AC</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">()</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">%</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">/</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">7</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">8</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">9</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">x</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">4</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">5</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">6</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">-</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">1</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">2</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">3</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">+</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">0</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">,</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">C</button>
+        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">=</button>
+      </div>
     </div>
-  </div>
-)
+  )
 }
 
 export default App
