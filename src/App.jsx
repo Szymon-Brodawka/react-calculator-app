676ba22d1fd07c77fb350a554e5bb635d009061f
Szymon Brodawka <szymbrod@gmail.com>
2024-05-29 15:26:49 +0200

    Add handling the operations

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,202 @@
 import { useReducer } from "react"
+import DigitButton from "./DigitButton"
+import OperationButton from "./OperationButton"
 
-const ACTIONS = 
+export const ACTIONS = 
 {
-  ADD_DIGIT: 'add-didgit',
+  ADD_DIGIT: 'add-digit',
   CHOOSE_OPERATION: 'choose-operation',
   CLEAR_ALL: 'clear-all',
   CLEAR_DIGIT: 'clear-digit',
-  EVALUATE: 'equal',
-  
+  EVALUATE: 'evaluate',
 }
 
-function reducer(state, action)
+function reducer(state, { type, payload })
 {
-  switch(action)
-  {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      if(state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false
+        }
+      }
 
+      if(payload.digit === '0' && state.currentOperand === '0') 
+        return state;
+      
+      if(payload.digit === '.' && state.currentOperand.includes('.'))
+      {
+        return state;
+      }
+      
+      return {
+          ...state,
+          currentOperand: `${state.currentOperand || ''}${payload.digit}`
+      };
+    
+    case ACTIONS.CHOOSE_OPERATION:
+      if(state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+
+      if(state.currentOperand == null){
+        return{
+          ...state,
+          operation: payload.operation,
+        }
+      }
+
+      if(state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null
+        }
+      }
+
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null
+      }
+    
+    case ACTIONS.CLEAR_ALL:
+      return {};
+    case ACTIONS.EVALUATE:
+      if(state.operation == null || state.currentOperand == null || state.previousOperand == null){
+        return state;
+      }
+
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      }
+    case ACTIONS.CLEAR_DIGIT:
+      if(state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null
+        };
+      }
+
+      if(state.currentOperand == null)
+        return state;
+
+      if(state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null };
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1)
+      };
   }
 }
 
-function App() 
+function evaluate({ currentOperand, previousOperand, operation })
 {
-  [
-    { previousOperand, currentOperand, operation }, 
-    dispatch
-  ] = useReducer(reducer, {})
+  const previous = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  
+  if(isNaN(previous) || isNaN(current)){
+    return '';
+  }
+
+  let computation = '';
+  
+  switch(operation){
+    case '+':
+      computation = previous + current;
+      break;
+    case '-':
+      computation = previous - current;
+      break;
+    case '/':
+      if(current == '0'){
+        break;
+      }
+
+      computation = previous / current;
+      break;
+    
+    case '*':
+      computation = previous * current;
+      break;
+  }
+
+  return computation.toString();
+}
+
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+});
+
+function formatOperand(operand) {
+  if(operand == null)
+    return;
+
+  const [ integer, decimal ] = operand.split('.');
+
+  if(decimal == null)
+    return INTEGER_FORMATTER.format(integer);
+
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}}`;
+
+}
+
+function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
   
   return (
     <div className="container mx-auto flex justify-center">
       <div className="bg-neutral-700 text-neutral-100 text-2xl grid grid-cols-4 gap-6 rounded-3xl p-5">
         <div className="flex flex-col gap-3 col-span-4 h-[8rem]">
-          <p className="ml-auto">{ previousOperand } { operation }</p>
-          <p className="ml-auto">{  currentOperand }</p>
+          <p className="ml-auto">{ formatOperand(previousOperand) } { operation }</p>
+          <p className="ml-auto">{ formatOperand(currentOperand) }</p>
         </div>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">AC</button>
+        <button 
+          className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700"
+          onClick={() => dispatch({ type: ACTIONS.CLEAR_ALL })}
+        >AC</button>
         <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">()</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">%</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">/</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">7</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">8</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">9</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">x</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">4</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">5</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">6</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">-</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">1</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">2</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">3</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">+</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">0</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">,</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">C</button>
-        <button className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700">=</button>
+        <OperationButton operation="%" dispatch={dispatch} />
+        <OperationButton operation="/" dispatch={dispatch} />
+        <DigitButton digit="7" dispatch={dispatch} />
+        <DigitButton digit="8" dispatch={dispatch} />
+        <DigitButton digit="9" dispatch={dispatch} />
+        <OperationButton operation="*" dispatch={dispatch} />
+        <DigitButton digit="4" dispatch={dispatch} />
+        <DigitButton digit="5" dispatch={dispatch} />
+        <DigitButton digit="6" dispatch={dispatch} />
+        <OperationButton operation="-" dispatch={dispatch} />
+        <DigitButton digit="1" dispatch={dispatch} />
+        <DigitButton digit="2" dispatch={dispatch} />
+        <DigitButton digit="3" dispatch={dispatch} />
+        <OperationButton operation="+" dispatch={dispatch} />
+        <DigitButton digit="0" dispatch={dispatch} />
+        <DigitButton digit="." dispatch={dispatch} />
+        <button 
+          className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700"
+          onClick={() => dispatch({ type: ACTIONS.CLEAR_DIGIT })}
+          >C</button>
+        <button 
+          className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700"
+          onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+          >=</button>
       </div>
     </div>
-  )
+  );
 }
 
-function calculate()
-{
-  //Get the number
-  //Get the operand
-  //Get next number, do calculations and show the effect on the screen
-}
-
-export default App
+export default App;
--- src/DigitButton.jsx
+++ src/DigitButton.jsx
@@ -2,7 +2,11 @@ import { ACTIONS } from "./App";
 
 export default function DigitButton({ dispatch, digit })
 {
-    return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: digit })}>
+    return ( <button 
+            onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+            className="bg-sky-800 rounded-xl py-2 px-4 text-center hover:bg-sky-700"
+           >
         {digit}
-    </button>
+        </button>
+    )
 }
